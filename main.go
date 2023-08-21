356384dbb0d4a56b8fdb7b7c6c8c4b0982e28ef8
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2023-08-22 08:14:04 +0900

    Show image

--- main.go
+++ main.go
@@ -1,28 +1,64 @@
 package main
 
 import (
+	"bytes"
+	"image"
+	_ "image/png"
+
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 320
+	screenHeight = 240
+
+	frameOX     = 0
+	frameOY     = 32
+	frameWidth  = 32
+	frameHeight = 32
+	frameCount  = 8
+)
+
+var (
+	mikuImage *ebiten.Image
+)
+
+type Game struct {
+	count int
+}
 
 func (g *Game) Update() error {
+	g.count++
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	i := (g.count / 5) % frameCount
+	sx, sy := frameOX+i*frameWidth, frameOY
+	screen.DrawImage(mikuImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), op)
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	mikuImage = ebiten.NewImageFromImage(img)
+
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
